2a5451040906fbb2c85b199baa86de137464e762
Tyler Lum <tylergwlum@gmail.com>
2019-09-01 23:12:00 -0700

    Procedural implementation of displaying names

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForEach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Print all names 1
+            List<string> names = new List<string>();
+            names.Add("Steve");
+            names.Add("Jon");
+            names.Add("Mary");
+            names.Add("Julie");
+
+            DisplayName(names);
+            Console.ReadLine();
+
+
+            // Print all names 2
+            List<string> otherNames = new List<string>()
+            {
+                "Tom",
+                "Dick",
+                "Harry"
+            };
+
+            DisplayName(otherNames);
+            Console.ReadLine();
+
+
+        }
+
+        static void DisplayName(List<string> names)
+        {
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+            
+        }
+    }
+}
